4d5ccf5fcdaac9538df99168ac286b0284d289d9
shivamt27 <42462537+shivamt27@users.noreply.github.com>
2018-12-03 14:36:08 +0530

    Add files via upload

---
+++ src/java/FeedbackServlet.java
@@ -0,0 +1,98 @@
+ import java.io.*;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Yash
+ */
+public class FeedbackServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            Class.forName("com.mysql.cj.jdbc.Driver");   
+           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb?useSSL=false","root","shivay10");
+           
+           PreparedStatement pstmt=conn.prepareStatement("insert into abc values(?,?,?)");
+           String s1 = request.getParameter("Username");
+           String s2 = request.getParameter("Email");
+           String s3 = request.getParameter("Password");
+           
+           
+           pstmt.setString(1,s2);
+           pstmt.setString(2,s1);
+            pstmt.setString(3,s3);
+           
+           
+          int i = pstmt.executeUpdate();
+          if(i > 0){
+          out.println("Booking successfull");
+          }
+          else{
+              out.println("Not Successfull");
+          }
+        }
+    catch(Exception e)
+    {
+        System.out.println(e);
+        
+    }
+   
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
